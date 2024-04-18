aaa7fe2843ed508419a3504cc0454ddf1bd91f8a
Nicholasclifford <60443900+Nicholasclifford@users.noreply.github.com>
2024-04-18 20:38:09 +0000

    fixing errors
    fixing errors and added more files

--- App/controllers/routine.py
+++ App/controllers/routine.py
@@ -1,11 +1,11 @@
 from App.models import routine
 from App.database import db
 
-    def add_exercise(self, exercise):
-        self.exercises.append(exercise)
+def add_exercise(self, exercise):
+  self.exercises.append(exercise)
 
-    def remove_exercise(self, exercise):
-        self.exercises.remove(exercise)
+def remove_exercise(self, exercise):
+  self.exercises.remove(exercise)
 
-    def get_exercises(self):
-        return self.exercises
+def get_exercises(self):
+  return self.exercises
